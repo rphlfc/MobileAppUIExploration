5bf87459817033f25ac8b33d753caf5b496f19f2
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-07-30 16:05:46 -0300

    Initial Commit

---
+++ MobileAppUIExploration/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MobileAppUIExploration
+//
+//  Created by Raphael Cerqueira on 30/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
